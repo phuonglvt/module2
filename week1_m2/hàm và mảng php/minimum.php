b9417dbe403d0ce4ec1ccdaba55984b2bc3ef5ea
phuonglvt <phuongtrucluu12082000@gmail.com>
2019-04-04 10:35:14 +0700

    tuan1

---
+++ week1_m2/hàm và mảng php/minimum.php
@@ -0,0 +1,9 @@
+<?php
+$Mang = array(1,3,-3,15,7,-25);
+$min = $Mang[0];
+for ($i= 1;$i< count($Mang) ; $i++) {
+    if ($Mang[$i] < $min  )
+        $min = $Mang[$i] ;
+}
+echo "Giá trị nhỏ nhất của mảng là: ". $min;
+?>
